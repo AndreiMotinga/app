985952c378ed1e7c9ef4868b361c04e09e2861c6
Andrei Motinga <andrew.motinga@gmail.com>
2018-03-29 18:42:42 -0400

    add router

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './Footer';
 import Stat from './Stat';
 import StatsList from './StatsList';
 
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 import Topics from './pages/Topics';
@@ -27,6 +28,32 @@ class App extends Component {
           <Stat />
           <Stat />
         </div>
+
+        <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+              <li>
+                <Link to="/posts/345">Post 345</Link>
+              </li>
+            </ul>
+
+            <hr />
+
+            <Route exact path="/home" component={Home} />
+            <Route path="/posts/:id" component={PostsShow} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
+          </div>
+        </Router>
 
         <div>
           <StatsList />
