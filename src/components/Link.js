67019b18e481ebcfdaecf113abd62fe31fc8e120
Andrei Motinga <andrew.motinga@gmail.com>
2018-03-31 14:32:02 -0400

    asdf asideasfd

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 const Link = ({ active, children, onClick }) => {
   if (active) {
     return <span>{children}</span>;
@@ -16,9 +17,11 @@ const Link = ({ active, children, onClick }) => {
     </a>
   );
 };
+
 Link.propTypes = {
   active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired
 };
+
 export default Link;
