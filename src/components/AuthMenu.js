f8c2bfd7c7bda77e0ea9046d42594ad0065dd278
Andrei Motinga <andrew.motinga@gmail.com>
2018-03-31 19:02:10 -0400

    add signup

---
+++ src/components/AuthMenu.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Button from 'material-ui/Button';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import IconButton from 'material-ui/IconButton';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import { Link } from 'react-router-dom';
+
+class AuthMenu extends React.Component {
+  state = {
+    auth: false,
+    anchorEl: null
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClick = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { anchorEl, auth } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className="Nav_right_item">
+        {!auth && (
+          <Button onClick={() => (document.location = '/users/sign_in')}>
+            Sign in
+          </Button>
+        )}
+
+        {auth && (
+          <div>
+            <IconButton
+              aria-owns={open ? 'profile' : null}
+              aria-haspopup="true"
+              onClick={this.handleMenu}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="profile"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={open}
+              onClose={this.handleClose}
+            >
+              <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+              <MenuItem onClick={this.handleClose}>My account</MenuItem>
+            </Menu>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default AuthMenu;
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,93 +4,25 @@ import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
-import IconButton from 'material-ui/IconButton';
-import AccountCircle from 'material-ui-icons/AccountCircle';
-import Switch from 'material-ui/Switch';
-import { FormControlLabel, FormGroup } from 'material-ui/Form';
-import Menu, { MenuItem } from 'material-ui/Menu';
 import logo from './logo.svg';
 import Tickers from 'components/Tickers';
 import SimpleMenu from 'components/SimpleMenu';
+import AuthMenu from 'components/AuthMenu';
 import CSS from 'config/css_variables';
 
-class MenuAppBar extends React.Component {
-  state = {
-    auth: false,
-    anchorEl: null
-  };
-
-  handleChange = (event, checked) => {
-    this.setState({ auth: checked });
-  };
-
-  handleMenu = event => {
-    this.setState({ anchorEl: event.currentTarget });
-  };
-
-  handleClose = () => {
-    this.setState({ anchorEl: null });
-  };
-
+class Nav extends React.Component {
   render() {
     const { classes } = this.props;
-    const { auth, anchorEl } = this.state;
-    const open = Boolean(anchorEl);
 
     return (
       <div className={classes.root}>
-        <FormGroup>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={auth}
-                onChange={this.handleChange}
-                aria-label="LoginSwitch"
-              />
-            }
-            label={auth ? 'Logout' : 'Login'}
-          />
-        </FormGroup>
         <AppBar position="static">
           <Toolbar>
             <img src={logo} className="App-logo" alt="logo" />
             <Tickers />
             <div className={classes.spacer} />
             <SimpleMenu />
-            <SimpleMenu />
-            <SimpleMenu />
-
-            {!auth && <a className={classes.login}>login</a>}
-
-            {auth && (
-              <div>
-                <IconButton
-                  aria-owns={open ? 'menu-appbar' : null}
-                  aria-haspopup="true"
-                  onClick={this.handleMenu}
-                  color="inherit"
-                >
-                  <AccountCircle />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorEl}
-                  anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right'
-                  }}
-                  open={open}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                </Menu>
-              </div>
-            )}
+            <AuthMenu />
           </Toolbar>
         </AppBar>
       </div>
@@ -98,7 +30,7 @@ class MenuAppBar extends React.Component {
   }
 }
 
-MenuAppBar.propTypes = {
+Nav.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
@@ -117,4 +49,4 @@ const styles = {
   }
 };
 
-export default withStyles(styles)(MenuAppBar);
+export default withStyles(styles)(Nav);
