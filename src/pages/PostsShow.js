7c43933527924db0e5e8c8c502733e8f4ca59a52
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-16 13:25:04 -0400

    hook post show to post

--- src/pages/PostsShow.js
+++ src/pages/PostsShow.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import axios from 'axios';
 
-const PostsShow = ({ match }) => (
-  <div>
-    <h2>Post id is {match.params.id}</h2>
-  </div>
-);
+class PostsShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      post: {}
+    };
+  }
+
+  componentDidMount() {
+    const id = this.props.match.params.id;
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/posts/${id}`)
+      .then(res => {
+        this.setState({ post: res.data });
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+  render() {
+    const { post } = this.state;
+    return (
+      <div>
+        <h2>Post id is {post.id}</h2>
+        <h2>{post.title}</h2>
+      </div>
+    );
+  }
+}
 
 export default PostsShow;
