e51b25fcbdc5bfcf254827e65b06dfcbdd4ecebc
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-18 14:32:09 -0400

    prevent auth from rendering if user signed in

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -4,13 +4,28 @@
 import React from 'react';
 import SigninForm from './SigninForm';
 import SignupForm from './SignupForm';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-const Auth = () => (
-  <div>
-    <SigninForm />
-    <br />
-    <SignupForm />
-  </div>
-);
+const Auth = ({ auth }) => {
+  const isSignedIn = !!auth.currentUser.email;
 
-export default Auth;
+  // prevent from rendering if user already signed in
+  if (isSignedIn) {
+    return <Redirect to="/" />;
+  }
+
+  return (
+    <div>
+      <SigninForm />
+      <br />
+      <SignupForm />
+    </div>
+  );
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps)(Auth);
