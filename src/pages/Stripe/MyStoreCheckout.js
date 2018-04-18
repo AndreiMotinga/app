129e7c1d5af794dbb9ced930b069371c2b6b3976
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-18 17:22:04 -0400

    merge into one

--- src/pages/Stripe/MyStoreCheckout.js
+++ src/pages/Stripe/MyStoreCheckout.js
@@ -1,16 +1,29 @@
 import React from 'react';
-import { Elements } from 'react-stripe-elements';
+import { Elements, injectStripe, CardElement } from 'react-stripe-elements';
 
-import InjectedCheckoutForm from './CheckoutForm';
+// import InjectedCheckoutForm from './CheckoutForm';
 
 class MyStoreCheckout extends React.Component {
+  handleSubmit = ev => {
+    ev.preventDefault();
+    this.props.stripe.createToken({}).then(({ token }) => {
+      console.log('Received Stripe token:', token);
+    });
+  };
+
   render() {
     return (
       <Elements>
-        <InjectedCheckoutForm />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Card details
+            <CardElement style={{ base: { fontSize: '18px' } }} />
+          </label>
+          <button>Confirm order</button>
+        </form>
       </Elements>
     );
   }
 }
 
-export default MyStoreCheckout;
+export default injectStripe(MyStoreCheckout);
