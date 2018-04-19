b060e647919a5bb48fa664817f48dacdeadfd6cf
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-19 19:11:44 -0400

    style table

--- src/components/StrategiesShowTable.js
+++ src/components/StrategiesShowTable.js
@@ -1,62 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
-import Table, {
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow
-} from 'material-ui/Table';
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
 import Paper from 'material-ui/Paper';
 
-function StrategiesShowTable(props) {
-  const { classes } = props;
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-    'YTD'
-  ];
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 2,
+    marginBottom: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+});
 
-  const data = [
-    [
-      '2015',
-      'null',
-      'null',
-      'null',
-      'null',
-      'null',
-      'null',
-      0,
-      3,
-      10,
-      4.3,
-      -5.6,
-      9,
-      9
-    ],
-    ['2016', 0, 0, 0, 0, 1.7, -0.5, 2, 3, 10, 4.3, -5.6, 9, 9],
-    ['2017', 0, 0, 0, 4, 1.7, -0.5, 2, 3, 10, 4.3, -5.6, 9, 9],
-    ['2018', -4.5, -2.1, 1.0, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
+
+const data = [
+  ['2015', 'null', 'null', 'null', 'null', 'null', 'null', 0, 3, 10, 4.3, -5.6, 9, 9],
+  ['2016', 0, 0, 0, 0, 1.7, -0.5, 2, 3, 10, 4.3, -5.6, 9, 9],
+  ['2017', 0, 0, 0, 4, 1.7, -0.5, 2, 3, 10, 4.3, -5.6, 9, 9],
+  ['2018', -4.5, -2.1, 1.0, 1.5, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+  'YTD'
+];
+
+
+function SimpleTable(props) {
+  const { classes } = props;
 
   return (
-    <Paper>
-      <Table className={classes.root}>
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell padding="checkbox" />
+            <TableCell />
             {months.map((m, i) => (
-              <TableCell key={i} padding="checkbox" numeric>
+              <TableCell key={i} numeric padding="checkbox">
                 {m}
               </TableCell>
             ))}
@@ -83,17 +78,8 @@ function StrategiesShowTable(props) {
   );
 }
 
-StrategiesShowTable.propTypes = {
-  classes: PropTypes.object.isRequired
+SimpleTable.propTypes = {
+  classes: PropTypes.object.isRequired,
 };
 
-const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 2,
-    marginBottom: theme.spacing.unit * 2,
-    overflowX: 'auto'
-  }
-});
-
-export default withStyles(styles)(StrategiesShowTable);
+export default withStyles(styles)(SimpleTable);
