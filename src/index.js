5e0aeb0f0e1b57e73ee7e7ee47d9c8ea2cdf7b2d
Andrei Motinga <andrew.motinga@gmail.com>
2018-04-21 20:41:49 -0400

    move index.css to config

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import registerServiceWorker from 'config/registerServiceWorker';
 import store from 'config/store';
+import './config/index.css';
 
 import App from 'components/App';
-import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
